9110f195101e065a5f85f744070142d280f5a48f
zhangjun <jun.zhang@medishare.cn>
2016-08-29 09:12:12 +0800

    格式化代码

--- selectColor/js/Rect.js
+++ selectColor/js/Rect.js
@@ -1,24 +1,24 @@
-function Rect(n,color,RectColor,canvasW,canvasH){
+function Rect(n, color, RectColor, canvasW, canvasH) {
 	createjs.Shape.call(this);
-	this.setRectType=function(type){
-		this._RectType=type;
-		switch(type){
+	this.setRectType = function (type) {
+		this._RectType = type;
+		switch (type) {
 			case 1:
-			this.setColor(color);
-			break;
+				this.setColor(color);
+				break;
 			case 2:
-			this.setColor(RectColor);
-			break;
+				this.setColor(RectColor);
+				break;
 		}
 	}
-	this.setColor=function(colorString){
+	this.setColor = function (colorString) {
 		this.graphics.beginFill(colorString);
-		this.graphics.drawRect(0,0,canvasW/n-5,canvasH/n-5);
+		this.graphics.drawRect(0, 0, canvasW / n - 5, canvasH / n - 5);
 		this.graphics.endFill();
 	}
-	this.getRectType=function(){
+	this.getRectType = function () {
 		return this._RectType;
 	}
 	this.setRectType(1);
 }
-Rect.prototype=new createjs.Shape();
+Rect.prototype = new createjs.Shape();
